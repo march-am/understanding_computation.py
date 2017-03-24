edb5e28f2201b5ee50a15d2c67264eaa39d0a33c
march-am <myredblood@gmail.com>
2017-03-24 22:18:56 +0900

    Number, Add, Multiplyの定義

--- operational_semantics/src/small-step_semantics.py
+++ operational_semantics/src/small-step_semantics.py
@@ -1,1 +1,15 @@
 #small-step semantics
+
+class Number:
+  def __init__(self, value):
+    self.value = value
+
+class Add:
+  def __init__(self, left, right):
+    self.left = left
+    self.right = right
+
+class Multiply:
+  def __init__(self, left, right):
+    self.left = left
+    self.right = right
