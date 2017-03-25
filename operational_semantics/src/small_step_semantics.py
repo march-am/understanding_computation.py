68eb45e4ecebe018a53c7b1c69b708220eb4f94a
march-am <myredblood@gmail.com>
2017-03-25 15:20:32 +0900

    代入の実装

--- operational_semantics/src/small_step_semantics.py
+++ operational_semantics/src/small_step_semantics.py
@@ -93,6 +93,31 @@ class Variable(Expression):
     return environment[self.name]
 
 
+class DoNothing(Expression):
+  def __init__(self):
+    self.reducible = False
+
+  def str(self):
+    return 'Do-Nothing'
+
+
+class Assign(Expression):
+  def __init__(self, name, expression):
+    self.name = name
+    self.expression = expression
+    self.reducible = True
+
+  def str(self):
+    return '{} = {}'.format(self.name, self.expression.str())
+
+  def reduce_exp(self, environment):
+    if self.expression.reducible:
+      return [Assign(self.name, self.expression.reduce_exp(environment)), environment]
+    else:
+      # updateすると元配列を破壊するのでdictコンストラクタで新しいdictを返す
+      return [DoNothing(), dict(environment.items() + [(self.name, self.expression)])]
+
+
 class Machine:
   def __init__(self, expression, environment):
     self.expression = expression
