62e3bb6ac08a2da3d98535cb4fb8c4dc98217afe
march-am <myredblood@gmail.com>
2017-05-18 09:55:20 +0900

    optimize with definitions

--- operational_semantics/src/small_step_semantics.py
+++ operational_semantics/src/small_step_semantics.py
@@ -1,144 +1,94 @@
 # -*- coding: utf-8 -*-
-#small-step semantics
+# small-step semantics
 
-class Expression:
-  def __str__(self):
-    return '«{}»'.format(self.str())
-  def __repr__(self):
-    return '«{}»'.format(self.str())
-  def __eq__(self, other):
-    if other is None or type(self) != type(other): return False
-    return self.__dict__ == other.__dict__
+import sys,os
+sys.path.append(os.path.abspath('../../'))
+from definitions import *
 
 
-class Statement(Expression):
-  pass
+class Number(Number):
+  def reducible(self):
+    return False
 
 
-class Number(Expression):
-  def __init__(self, value):
-    self.value = value
-    self.reducible = False
-
-  def str(self):
-    return str(self.value)
-
-
-class Add(Expression):
-  def __init__(self, left, right):
-    self.left = left
-    self.right = right
-    self.reducible = True
-
-  def str(self):
-    return '{} + {}'.format(self.left.str(), self.right.str())
+class Add(Add):
+  def reducible(self):
+    return True
 
   def reduce_exp(self, environment):
-    if self.left.reducible:
+    if self.left.reducible():
       return Add(self.left.reduce_exp(environment), self.right)
-    elif self.right.reducible:
+    elif self.right.reducible():
       return Add(self.left, self.right.reduce_exp(environment))
     else:
       return Number(self.left.value + self.right.value)
 
 
-class Multiply(Expression):
-  def __init__(self, left, right):
-    self.left = left
-    self.right = right
-    self.reducible = True
-
-  def str(self):
-    return '{} * {}'.format(self.left.str(), self.right.str())
+class Multiply(Multiply):
+  def reducible(self):
+    return True
 
   def reduce_exp(self, environment):
-    if self.left.reducible:
+    if self.left.reducible():
       return Add(self.left.reduce_exp(environment), self.right)
-    elif self.right.reducible:
+    elif self.right.reducible():
       return Add(self.left, self.right.reduce_exp(environment))
     else:
       return Number(self.left.value * self.right.value)
 
 
-class Boolean(Expression):
-  def __init__(self, value):
-    self.value = value
-    self.reducible = False
-
-  def str(self):
-    return str(self.value)
+class Boolean(Boolean):
+  def reducible(self):
+    return False
 
 
-class LessThan(Expression):
-  def __init__(self, left, right):
-    self.left = left
-    self.right = right
-    self.reducible = True
-
-  def str(self):
-    return '{} < {}'.format(self.left.str(), self.right.str())
+class LessThan(LessThan):
+  def reducible(self):
+    return True
 
   def reduce_exp(self, environment):
-    if self.left.reducible:
+    if self.left.reducible():
       return LessThan(self.left.reduce_exp(environment), self.right)
-    elif self.right.reducible:
+    elif self.right.reducible():
       return LessThan(self.left, self.right.reduce_exp(environment))
     else:
       return Boolean(self.left.value < self.right.value)
 
 
-class Variable(Expression):
-  def __init__(self, name):
-    self.name = name
-    self.reducible = True
-
-  def str(self):
-    return str(self.name)
+class Variable(Variable):
+  def reducible(self):
+    return True
 
   def reduce_exp(self, environment):
     return environment[self.name]
 
 
-class DoNothing(Statement):
-  def __init__(self):
-    self.reducible = False
+class DoNothing(DoNothing):
+  def reducible(self):
+    return False
 
   def __eq__(self, other):
     return isinstance(self, type(DoNothing()))
 
-  def str(self):
-    return 'Do-Nothing'
 
-
-class Assign(Statement):
-  def __init__(self, name, expression):
-    self.name = name
-    self.expression = expression
-    self.reducible = True
-
-  def str(self):
-    return '{} = {}'.format(self.name, self.expression.str())
+class Assign(Assign):
+  def reducible(self):
+    return True
 
   def reduce_exp(self, environment):
-    if self.expression.reducible:
+    if self.expression.reducible():
       return [Assign(self.name, self.expression.reduce_exp(environment)), environment]
     else:
       # updateすると元配列を破壊するのでdictコンストラクタで新しいdictを返す
       return [DoNothing(), dict(environment.items() + [(self.name, self.expression)])]
 
 
-class If(Statement):
-  def __init__(self, condition, consequence, alternative):
-    self.condition = condition
-    self.consequence = consequence
-    self.alternative = alternative
-    self.reducible = True
-
-  def str(self):
-    return 'If ({}) {{ {} }} else {{ {} }}'.format(self.condition, self.consequence, self.alternative)
+class If(If):
+  def reducible(self):
+    return True
 
   def reduce_exp(self, environment):
-    if self.condition.reducible:
+    if self.condition.reducible():
       return [If(self.condition.reduce_exp(environment), self.consequence, self.alternative), environment]
     else:
       if self.condition == Boolean(True):
@@ -147,14 +97,9 @@ class If(Statement):
         return [self.alternative, environment]
 
 
-class Sequence(Statement):
-  def __init__(self, first, second):
-    self.first = first
-    self.second = second
-    self.reducible = True
-
-  def str(self):
-    return '{}; {}'.format(self.first, self.second)
+class Sequence(Sequence):
+  def reducible(self):
+    return True
 
   def reduce_exp(self, environment):
     if self.first == DoNothing():
@@ -164,14 +109,9 @@ class Sequence(Statement):
       return [Sequence(reduced_first, self.second), reduced_environment]
 
 
-class While(Statement):
-  def __init__(self, condition, body):
-    self.condition = condition
-    self.body = body
-    self.reducible = True
-
-  def str(self):
-    return 'While ({}) {{ {} }}'.format(self.condition, self.body)
+class While(While):
+  def reducible(self):
+    return True
 
   def reduce_exp(self, environment):
     return [If(self.condition, Sequence(self.body, self), DoNothing()), environment]
@@ -186,7 +126,7 @@ class Machine:
     self.statement, self.environment = self.statement.reduce_exp(self.environment)
 
   def run(self):
-    while self.statement.reducible:
+    while self.statement.reducible():
       print('{}, {}'.format(self.statement, self.environment))
       self.step()
     print('{}, {}'.format(self.statement, self.environment))
